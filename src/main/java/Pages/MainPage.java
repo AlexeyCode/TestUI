a2772500db2941ecc39a0ea20d7177d9a67c502b
ayudin <erasecorp@hotmail.com>
2019-04-12 11:01:58 +0300

    Initial commit

---
+++ src/main/java/Pages/MainPage.java
@@ -0,0 +1,20 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MainPage {
+    private WebDriver driver;
+
+    public MainPage( WebDriver driver ){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//a[text()= \"A/B Testing\"]")
+    WebElement ABLink;
+
+
+}
---
+++ src/test/java/TestUI.java
@@ -0,0 +1,14 @@
+import Pages.MainPage;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+public class TestUI {
+    private WebDriver driver;
+    private MainPage mainPage;
+
+    @Test
+    void test(){
+        System.out.println("SOme");
+    }
+
+}
